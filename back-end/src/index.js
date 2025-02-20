f8b27595c7fe6bd16ea29eb5d666083eef9838c5
Jean Pissineli <jpbpissineli05@gmail.com>
2025-02-20 17:46:46 -0300

    alguns commits

---
+++ back-end/src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express()
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("API FUNCIONANDO")
+})
+
+app.post("/jogar-dado", (req, res) => {
+    const tipoDado = req.body.tipo_dado;
+    const tiposValidos = ['D2', 'D4', 'D6', 'D8', 'D10', 'D12', 'D20', 'D100'];
+
+    if (!tiposValidos.includes(tipoDado)) {
+        return res.status(400).json({ erro: "Tipo de dado inválido." });
+    }
+
+    let numeroFaces = parseInt( tipoDado.slice(1, tipoDado.length) )
+    const resultado = Math.floor(Math.random() * numeroFaces) + 1;
+
+    console.log("POST: /jogar-dado")
+    console.log({ resultado: resultado })
+    res.json({ resultado: resultado });
+})
+
+app.listen(3000, () => {
+    console.log("Servidor iniciado: http://localhost:3000/")
+})
